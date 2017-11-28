c17929902c017bb885ee15bdb08e225d25e68d39
Marimuthu, Sudhakar <Sudhakar.Marimuthu@pearson.com>
2017-11-27 20:10:03 -0800

    new commit

--- src/com/ik/recursion/session/SubSetString.java
+++ src/com/ik/recursion/session/SubSetString.java
@@ -23,8 +23,8 @@ public class SubSetString {
 		
 		char[] ch1 = {'a','b','c'};
 		char[] ch2 = {'d','e','p'};
-		char[] ch = {'1','2','3'};
-		printSubsetsMain(ch2);
+		char[] ch = {'2','4','5'};
+		printSubsetsMain(ch);
 	}
 	
 	static void subSet(String soFar, String rest, List<String> list){
@@ -32,11 +32,10 @@ public class SubSetString {
 			//System.out.println(soFar);
 			list.add(soFar);
 		}else{
-			subSet(soFar, rest.substring(1), list);
 			subSet(soFar + rest.charAt(0), rest.substring(1), list);
+			subSet(soFar, rest.substring(1), list);
 		}
 	}
-	
 	static void printSubsetsMain(char[] inp){
 		char[] out = new char[inp.length];
 		printSubsets(inp, 0, out, 0);
@@ -44,11 +43,26 @@ public class SubSetString {
 	static void printSubsets(char[] array, int i, char[]output, int k) {
 		if (i == array.length){
             print(output, k);return;
-         }
+        }
+		
 		printSubsets(array, i+1, output, k);
-		output[k] = array[i]; 
+		output[k] = array[i];
 		printSubsets(array, i+1, output, k+1);
+		
 	}
+	
+	static void printSubsetsWithGivenSum(int[] array, int i, int[]output, int k) {
+		if (i == array.length){
+			
+            print(output, k);
+            return;
+         }
+		
+		printSubsetsWithGivenSum(array, i+1, output, k);
+		output[k] = array[i]; 
+		printSubsetsWithGivenSum	(array, i+1, output, k+1);
+	}
+	
 	static void printSubsets1(char[] array, int i, char[]output, int k) {
 		if (i == array.length){
             print(output, k);return;
@@ -62,6 +76,13 @@ public class SubSetString {
 		System.out.println("i-C : "+ i + " k : "+ k );
 	}
 	static void print(char[] output, int k){
+		//Print output[0...k-1]
+		for (int z = 0; z < k; z++) {
+			System.out.print("" + output[z] + " ");
+		}
+		System.out.println("");
+	}
+	static void print(int[] output, int k){
 		//Print output[0...k-1]
 		for (int z = 0; z < k; z++) {
 			System.out.print("" + output[z] + " ");

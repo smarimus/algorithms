82d36383d2a1c5736637c623d900c126540455e2
smarimus <sudhakarmuthu@gmail.com>
2018-03-26 11:26:54 -0700

    new commit

--- src/com/ik/tree/part2/TreeIterator.java
+++ src/com/ik/tree/part2/TreeIterator.java
@@ -2,12 +2,35 @@ package com.ik.tree.part2;
 
 import java.util.Stack;
 
+import com.ik.tree.session.BST;
 import com.ik.tree.session.Node;
 
 public class TreeIterator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		BST bst = new BST();
+		Node<Integer> root = new Node<Integer>(25);
+		
+		bst.add(root, 18);
+		bst.add(root, 20);
+		bst.add(root, 19);
+		bst.add(root, 24);
+		bst.add(root, 10);
+		bst.add(root, 40);
+		bst.add(root, 36);
+		bst.add(root, 38);
+		bst.add(root, 37);
+		bst.add(root, 30);
+		bst.add(root, 55);
+		bst.add(root, 42);
+		
+		TreeIterator ti = new TreeIterator(root);
+		
+		while(ti.hasNext()){
+			System.out.println(ti.next().getValue());
+		}
 
 	}
 	
@@ -16,32 +39,39 @@ public class TreeIterator {
 	TreeIterator(Node<Integer> root){
 		if(root == null) return;
 		
-		if(root.getLeft() != null){
-			s.push(root.getLeft());
-			root = root.getLeft();
+		s.push(root);
+		Node<Integer> left = root.getLeft();
+		while(left != null){
+			s.push(left);
+			left = left.getLeft();
 		}
 	}
 	
 	public Node<Integer> next(){
+		Node<Integer> result = null;
 		if(!s.isEmpty()){
 			
 			Node<Integer> node = s.pop();
-			if(node.getRight() != null){
-				Node<Integer> right = node.getRight();
-				while(right != null){
-					
+			Node<Integer> innerNode = node.getRight();
+			
+			while(innerNode != null){
+				s.push(innerNode);
+				
+				if(innerNode.getLeft() != null){
+					//s.push(innerNode.getLeft());
+					innerNode = innerNode.getLeft();
+				}else{
+					innerNode = innerNode.getRight();
 				}
 			}
+			result = node;
 		}
-		return null;
+		
+		return result;
 	}
 	
 	public boolean hasNext(){
-		if(!s.isEmpty()){
-			return true;
-		}
-		
-		return false;
+		return !s.isEmpty();
 	}
 }
 

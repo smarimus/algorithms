b822f8cb43006363117302a95bcfaecaf93fae1c
Sudhakar Marimuthu <5806942+smarimus@users.noreply.github.com>
2022-06-28 21:36:51 -0700

    Update MinimumWindowSubstring_76.java
    min window changes

--- src/com/leetcode/slidingwindow/MinimumWindowSubstring_76.java
+++ src/com/leetcode/slidingwindow/MinimumWindowSubstring_76.java
@@ -1,178 +1,253 @@
 package com.leetcode.slidingwindow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class MinimumWindowSubstring_76 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String s = "";
 		String t = "ABC";
+
+		// String s = "a";
+		// String t = "a";
+
+		// System.out.println(s.substring(0, s.length()));
+		// System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+		System.out.println(findMinWindow("a", "a"));
+		System.out.println(findMinWindow("aa", "aa"));
+		System.out.println(findMinWindow("bbaa", "aba"));
+		System.out.println(findMinWindow("ZABAACBAB", "ABC"));
+		System.out.println(findMinWindow("ADOBECODEBANC", "ABC"));
 		
-		//String s = "a";
-		//String t = "a";
+		System.out.println("==========");
 		
-		//System.out.println(s.substring(0, s.length()));
-		//System.out.println(minWindow("ADOBECODEBANC", "ABC"));
-		//System.out.println(minWindow("a", "a"));
-		//System.out.println(minWindow("aa", "aa"));
-		System.out.println(minWindow1("bbaa", "aba"));
+		System.out.println(minWindow("a", "a"));
+		System.out.println(minWindow("aa", "aa"));
+		System.out.println(minWindow("bbaa", "aba"));
+		System.out.println(minWindow("ZABAACBAB", "ABC"));
+		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
 	}
-	
-	static class Result {
-		int start;
-		int end;
+
+	static class Pair {
+		int left;
+		int right;
 		int len;
-		Result(){}
-		Result(int start, int end, int len){
-			this.start = start;
-			this.end = end;
-			this.len =len;
+
+		Pair(int l, int r) {
+			left = l;
+			right = r;
 		}
 	}
-	
+
 	public static String minWindow(String s, String t) {
-		
-		if(s == null || t == null || t.length() == 0) return "";
-		if(t.length() > s.length()) return "";
-		if(s.equals(t)) return t;
-		
-		Set<Character> set =  new TreeSet<Character>();
-		
+
+		if (s.length() == 0 || t.length() == 0) {
+			return "";
+		}
+
+		// Dictionary which keeps a count of all the unique characters in t.
+		Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+		for (int i = 0; i < t.length(); i++) {
+			int count = dictT.getOrDefault(t.charAt(i), 0);
+			dictT.put(t.charAt(i), count + 1);
+		}
+
+		// Number of unique characters in t, which need to be present in the desired
+		// window.
+		int required = dictT.size();
+
+		// Left and Right pointer
+		int l = 0, r = 0;
+
+		// formed is used to keep track of how many unique characters in t
+		// are present in the current window in its desired frequency.
+		// e.g. if t is "AABC" then the window must have two A's, one B and one C.
+		// Thus formed would be = 3 when all these conditions are met.
+		int formed = 0;
+
+		// Dictionary which keeps a count of all the unique characters in the current
+		// window.
+		Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+		// ans list of the form (window length, left, right)
+		int[] ans = { -1, 0, 0 };
+
+		while (r < s.length()) {
+			// Add one character from the right to the window
+			char c = s.charAt(r);
+			int count = windowCounts.getOrDefault(c, 0);
+			windowCounts.put(c, count + 1);
+
+			// If the frequency of the current character added equals to the
+			// desired count in t then increment the formed count by 1.
+			if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+				formed++;
+			}
+
+			// Try and contract the window till the point where it ceases to be 'desirable'.
+			while (l <= r && formed == required) {
+				c = s.charAt(l);
+				// Save the smallest window until now.
+				if (ans[0] == -1 || r - l + 1 < ans[0]) {
+					ans[0] = r - l + 1;
+					ans[1] = l;
+					ans[2] = r;
+				}
+
+				// The character at the position pointed by the
+				// `Left` pointer is no longer a part of the window.
+				windowCounts.put(c, windowCounts.get(c) - 1);
+				if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+					formed--;
+				}
+
+				// Move the left pointer ahead, this would help to look for a new window.
+				l++;
+			}
+
+			// Keep expanding the window once we are done contracting.
+			r++;
+		}
+
+		return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+	}
+
+	public static String findMinWindow(String s, String t) {
+
+		Set<Character> set = new TreeSet();
 		char[] chArr = t.toCharArray();
-		for(Character c: chArr){
+		for (Character c : chArr) {
 			set.add(c);
 		}
-		
-		int right=0;
-		for(int j=0; j<s.length(); j++){
-			if(set.contains(s.charAt(j))){
-				right=j;
+
+		return shortestContainingSubArray(s, s.toCharArray(), set);
+	}
+
+	public static String shortestContainingSubArray(String s, char[] array, Set<Character> set) {
+		int left;
+		for (left = 0; left < array.length; ++left) {
+			if (set.contains(array[left]))
 				break;
-			}
 		}
-		
-		Map<Character, Integer> subSet = new HashMap<Character, Integer>();
-		
-		Result result = null;
-		int missing = set.size();
-		int left=right;
-		
-		for (right = left+1; right < s.length(); right++) {
-			char k = s.charAt(right);
-			
-			if(!set.contains(k)){
+		if (left == array.length) {
+			return "";
+		}
+		if (set.size() == 1) {
+			// return new Pair(left, left);
+			return s.substring(left);
+		}
+
+		Map<Character, Integer> map = new HashMap();
+		map.put(array[left], 1);
+		int missing = set.size() - 1;
+		Pair result = null;
+
+		for (int right = left + 1; right < array.length; ++right) {
+			char c = array[right];
+			if (!set.contains(c))
 				continue;
-			}
-			
-			Integer count = subSet.get(k);
-			if(count == 0){
+
+			int count = map.getOrDefault(c, 0);
+			map.put(c, count + 1);
+
+			if (count == 0)
 				missing--;
-			}
-			subSet.put(k, ++count);
-			if(missing >0){
+
+			if (missing > 0)
 				continue;
-			}
-			
-			if(result == null || right - left < result.end - result.start){
-				result = new Result(left, right, right-left);
-			}
-			
-			for (; left < right; left++) {
-				char leftChar = s.charAt(left);
-				if(!set.contains(leftChar)){
+
+			for (;; ++left) {
+				c = array[left];
+				if (!set.contains(c))
 					continue;
-				}
-				
-				//calculate length right - left
-				if(result == null || right - left < result.end - result.start){
-					result = new Result(left, right, right-left);
-				}
-				count = subSet.get(leftChar);
-				subSet.put(leftChar, --count);
-				
-				if(count == 0){
-					missing++;
-					left++;
+
+				if (result == null || right - left < result.right - result.left)
+					result = new Pair(left, right);
+
+				count = map.get(c);
+				map.put(c, --count);
+				if (count == 0) {
+					++left;
+					missing = 1;
 					break;
 				}
 			}
 		}
-		return (result !=  null && s.substring(result.start, result.end+1) != null) ?s.substring(result.start, result.end+1) : "" ;
+		return s.substring(result.left, result.right + 1);
 	}
-	
-	public static String minWindow1(String s, String t) {
 
-	      if (s.length() == 0 || t.length() == 0) {
-	          return "";
-	      }
 
-	      // Dictionary which keeps a count of all the unique characters in t.
-	      Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-	      for (int i = 0; i < t.length(); i++) {
-	          int count = dictT.getOrDefault(t.charAt(i), 0);
-	          dictT.put(t.charAt(i), count + 1);
-	      }
 
-	      // Number of unique characters in t, which need to be present in the desired window.
-	      int required = dictT.size();
+	public static String findMinWindow1(String s, String t) {
 
-	      // Left and Right pointer
-	      int l = 0, r = 0;
+		Set<Character> set = new TreeSet<Character>();
+		char[] chArr = t.toCharArray();
+		for (Character c : chArr) {
+			set.add(c);
+		}
 
-	      // formed is used to keep track of how many unique characters in t
-	      // are present in the current window in its desired frequency.
-	      // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-	      // Thus formed would be = 3 when all these conditions are met.
-	      int formed = 0;
+		return shortestContainingSubArray1(s, t, set);
+	}
 
-	      // Dictionary which keeps a count of all the unique characters in the current window.
-	      Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+	//ADOBECODEBANC
+	//ABC
+	public static String shortestContainingSubArray1(String s, String t, Set<Character> set) {
 
-	      // ans list of the form (window length, left, right)
-	      int[] ans = {-1, 0, 0};
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
 
-	      while (r < s.length()) {
-	          // Add one character from the right to the window
-	          char c = s.charAt(r);
-	          int count = windowCounts.getOrDefault(c, 0);
-	          windowCounts.put(c, count + 1);
+		char[] sArr = s.toCharArray();
+		int right=0;
+		for(int z=0; z<sArr.length; z++){
+			if (set.contains(sArr[z])){
+				right = z;
+			}
+		}
+		map.put(sArr[right], 1);
+		int missing = t.length()-1;
+		Pair result = null;
 
-	          // If the frequency of the current character added equals to the
-	          // desired count in t then increment the formed count by 1.
-	          if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-	              formed++;
-	          }
+		int left = right;
 
-	          // Try and contract the window till the point where it ceases to be 'desirable'.
-	          while (l <= r && formed == required) {
-	              c = s.charAt(l);
-	              // Save the smallest window until now.
-	              if (ans[0] == -1 || r - l + 1 < ans[0]) {
-	                  ans[0] = r - l + 1;
-	                  ans[1] = l;
-	                  ans[2] = r;
-	              }
+		for(; right< sArr.length; right++){
+			char c = sArr[right];
+			if(!set.contains(c)){
+				continue;
+			}
+			int count = map.getOrDefault(c, 0);
+			map.put(c, count+1);
 
-	              // The character at the position pointed by the
-	              // `Left` pointer is no longer a part of the window.
-	              windowCounts.put(c, windowCounts.get(c) - 1);
-	              if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-	                  formed--;
-	              }
+			if(count == 0 ){
+				missing--;
+			}
+			if(missing>0){
+				continue;
+			}
 
-	              // Move the left pointer ahead, this would help to look for a new window.
-	              l++;
-	          }
-
-	          // Keep expanding the window once we are done contracting.
-	          r++;   
-	      }
-
-	      return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
-	  }
+			for(; ; left++){
+				c = sArr[left];
+				if(!set.contains(c)){
+					continue;
+				}
+				if(result == null || left - right  < result.right - result.left){
+					result = new Pair(left, right);
+				}
+				
+				count = map.getOrDefault(c, 0);
+				if(count == 1){
+					map.put(c, count--);
+					left++;
+					missing=1;
+					break;
+				}
+			}
+		}
+		return s.substring(left, right+1);
+	}
 }
